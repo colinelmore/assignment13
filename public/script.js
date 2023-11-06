af1455592abfa37564b9a140e837489a58fa42f6
colinelmore <112006484+colinelmore@users.noreply.github.com>
2023-11-06 13:24:07 -0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 const showSports = async () => {
-    let response = await fetch("https://assignment13-9xi0.onrender.com/api/sports");
+    let response = await fetch("https://assignment13-tp9j.onrender.com/api/sports");
     let sportsJSON = await response.json();
     let sportDiv = document.getElementById("sport-div");
 
@@ -22,7 +22,7 @@ const showSports = async () => {
 
         let img = document.createElement("img");
         section.append(img);
-        img.src ="https://assignment13-9xi0.onrender.com/api/sports/" + sports.img;
+        img.src ="https://assignment13-tp9j.onrender.com/api/sports/" + sports.img;
 
         let colors = document.createElement("p");
         section.append(colors);
